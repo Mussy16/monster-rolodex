2304bb5e2dfac69dcf5496374167542243689e05
Tafadzwa <musinachirevotafadzwa@gmail.com>
2023-07-12 10:32:33 +0200

    Added the button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      name:"Mbizi"
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+           Hello {this.state.name}
+          </p>   
+          <button>Click me</button>
+        </header>
+      </div>
+    );
+  }  
+ }
 export default App;
