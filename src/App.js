708aa28e62f8e1c0b88620b9606ecc5b317688dc
Tafadzwa <musinachirevotafadzwa@gmail.com>
2023-07-22 15:42:14 +0200

    added css to the react file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
-import { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component{
-  constructor(){
-    super()
+class App extends Component {
+  constructor() {
+    super();
     this.state = {
-      name: {firstName:"Tafadzwa",lastNme:"Musinachirevo"},
-      company: "Glow"
-    }
+      monsters: [],
+      searchField: ''
+    };
   }
-  render(){
+  componentDidMount() {
+    console.log("ComponentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+        )
+      );
+  }
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return {searchField }
+    }
+    );
+  }
+  render() {
+    const {monsters ,searchField} = this.state;
+    const {onSearchChange} = this;
+    const FiltredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-           Hello {this.state.name.firstName},
-           {this.state.name.lastNme},
-           I work at {this.state.company}
-          </p>   
-          <button onClick={()=>{
-            this.setState({name :"Aphonso"})
-          }}>Click me</button>
-        </header>
+       <SearchBox onChangeHandler = {onSearchChange}
+       className = 'search-box'
+       placeholder = 'Search-monsters'
+       />
+        {/*FiltredMonsters.map((monster) => {
+          return <h1 key={monster.name}>{monster.name}</h1>;
+        })*/}
+        <CardList monsters ={ FiltredMonsters}/>
       </div>
     );
-  }  
- }
+  }
+}
 export default App;
